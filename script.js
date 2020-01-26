9dffe26e923cb3f9bc8d388128d84bbe890f2091
Luan <nguyenbaoluan1989@gmail.com>
2020-01-26 15:42:13 -0600

    operate func

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function subtract(a,b){return a-b;}
 function multiply(a,b){return a*b;}
 
 function divide(a,b){return b === 0 ? 'DIV BY 0' : a/b;}
+
+function operate(operator,num1,num2){
+    switch(operator){
+        case '+':
+            add(num1,num2);
+            break;
+        case '-':
+            subtract(num1,num2);
+            break;
+        case '*':
+            multiply(num1,num2);
+            break;
+        case '/':
+            divide(num1,num2);
+            break;
+        default:break;
+    }
+}
