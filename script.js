dde324451737e08ea5c240f012af83683edc4707
Luan <nguyenbaoluan1989@gmail.com>
2020-01-27 12:52:28 -0600

    change appendInput func for decimal btn, storeNum1 and storeNum2 funcs

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+let firstOperand = null;
+let secondOperand = null;
+let result = null;
+
 const input = document.querySelector('input');
 
 //clear func**********************************************
-function clear(){input.value = '';}
+function clear(){
+    input.value = '';
+    firstOperand = null;
+    secondOperand = null;
+    result = null;
+}
 //clear func**********************************************
 
 //call clear func on load
@@ -37,17 +46,47 @@ function operate(operator,num1,num2){
 }
 //func to execute an operation*****************************
 
+//func to append to display when num btn or decimal btn clicked
+function appendBtnInput(event){
+    //check if dec already exist
+    if(event.target.id==='decBtn' && input.value.indexOf('.') !== -1){
+        console.log('decimal already exist');
+        return;
+    }
+    else{input.value += event.target.value;}
+}
+
 const numBtns = Array.from(document.querySelectorAll('.numBtn'));
 
-//event listener for number buttons to change input display
-function appendBtnInput(event){input.value += event.target.value;}
+//event listener for number buttons
 numBtns.forEach(numBtn => numBtn.addEventListener('click',appendBtnInput));
-//event listener for number buttons to change input display
 
-const clearBtn = document.querySelector('.clearBtn');
+const clearBtn = document.querySelector('#clearBtn');
 
 //event listener for AC button
 clearBtn.addEventListener('click',clear);
 
+//func to store firstOperand and clear display
+function storeNum1(){
+    firstOperand = input.value;
+    input.value = '';
+    console.log('firstOperand: ' + firstOperand);
+}
+
+//func to store secondOperand and clear display
+function storeNum2(){
+    secondOperand = input.value;
+    input.value = '';
+    console.log('secondOperand: ' + secondOperand);
+}
+
 const opBtns = Array.from(document.querySelectorAll('.operatorBtn'));
+
+//event listeners for operation buttons
+opBtns.forEach(opBtn => opBtn.addEventListener('click',storeNum1));
+
+const equalBtn = document.querySelector('#equalBtn');
+
+//event listener for equal btn
+equalBtn.addEventListener('click',storeNum2);
 
