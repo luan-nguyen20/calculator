296dff4f1d8eda951e317e684081e8e17ebf869a
Luan <nguyenbaoluan1989@gmail.com>
2020-01-29 11:34:34 -0600

    appendToDisplay func

--- script.js
+++ script.js
@@ -83,8 +83,7 @@ clearBtn.addEventListener('click',clear);
 //call clear upon page loads
 window.onload = clear;
 
-//func to add numbers and decimal points to display
-function appendToDisplay(event){
+function appendToDisplay(){
     if(event.target.id==='decBtn'){ //add dec point only if
         if(input.value.indexOf('.') === -1){ //dec point doesn't exist
             input.value += event.target.value;
@@ -97,9 +96,8 @@ function appendToDisplay(event){
         input.value += event.target.value;
     }
 }
-//*************************************************************
 
-//numBtns event listener
+
 const numBtns = Array.from(document.querySelectorAll('.numBtn'));
 numBtns.forEach(numBtn => numBtn.addEventListener('click',appendToDisplay));
 
@@ -117,14 +115,6 @@ backBtn.addEventListener('click',back);
 
 //func to change between negative and positive
 function changeSign(){
-    //if charAt index 0 is not - , add - to front
-    //else, remove -
-    if(input.value[0] !== '-'){
-        input.value = '-' + input.value;
-    }
-    else{
-        input.value = input.value.substring(1,input.value.length);
-    }
 }
 //*************************************************************
 
@@ -169,83 +159,9 @@ function displayResult(resultValue){resultTxt.textContent = 'Result: ' + resultV
 //func to check if display is empty
 function displayIsEmpty(){return input.value === '';}
 
-function operatorBtnClicked(event){
-    let leftOperand, operator;
-    
-    operator = event.target.value;
-
-    operatorQueue.insert(operator); //insert operator to operator queue
-
-    if(resultStack.isEmpty()){ 
-        if(!displayIsEmpty()){ 
-            //if result stack is empty and display is not empty
-            //push input on result stack and clear display
-            resultStack.push(Number(input.value));
-            clearInputDisplay();
-        }
-    }
-    else{
-        if(!displayIsEmpty()){ //if both result stack and display are not empty,
-            //assign top of result stack to left operand
-            leftOperand = resultStack.pop(); 
-            //do calc with top of result stack and input, 
-            //then push result to top of result stack
-            resultStack.push(operate(leftOperand,Number(input.value),operator));
-            //show latest result
-            displayResult(resultStack.peek().toString());
-            //clear display screen
-            clearInputDisplay();
-            //remove operator from queue
-            operatorQueue.remove();
-        }
-        else { //if result stack is not empty and display is empty
-            //insert operator in operator queue
-            operatorQueue.insert(operator);
-        }
-    }
-}
-
-//operatorBtns event listeners
 const opBtns = document.querySelectorAll('.operatorBtn');
-opBtns.forEach(opBtn => opBtn.addEventListener('click',operatorBtnClicked));
-
-const addBtn = document.querySelector('#addBtn');
-const subBtn = document.querySelector('#subBtn');
-const multBtn = document.querySelector('#multBtn');
-const divBtn = document.querySelector('#divBtn');
 
 function equalBtnClicked(){
-    if(!displayIsEmpty()){ //if display is not empty
-        if(resultStack.isEmpty()){ //and result stack is empty
-            //display input as result and clear display
-            displayResult(input.value);
-            //resultStack.push(Number(input.value));
-            clearInputDisplay();
-        }
-        else{ //if both display and result stack are not empty
-            switch(operatorQueue.remove()){
-                case '+':
-                    addBtn.click();
-                    break;
-                case '-':
-                    subBtn.click();
-                    break;
-                case '*':
-                    multBtn.click();
-                    break;
-                case '/':
-                    divBtn.click();
-                    break;
-                default:break;
-            }
-        }
-    }
-    else{ //if display is empty
-        if(!resultStack.isEmpty()){ //and result stack is not empty
-            //display the latest result
-            displayResult(resultStack.pop().toString());
-        }
-    }
 }
 
 //equalBtn event listener
